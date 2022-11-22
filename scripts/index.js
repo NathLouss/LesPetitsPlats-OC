6a06b937ace879734e052418d85a94f26d0f9abb
nathalie loussouarn <natel@hotmail.com>
2022-11-22 22:10:00 +0100

    searchBar eventlistener and start filter btn integration

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 import { getRecipes } from "./api/services.js";
 import { recipeFactory } from "./factory/recipeFactory.js";
+import { searchRecipe } from "./utils/searchBar.js";
 
+// création et affichage des cards recette via la recipeFactory
 async function displayRecipes(datas) {
   const recipesSection = document.getElementById("recipes");
 
@@ -10,7 +12,6 @@ async function displayRecipes(datas) {
     recipesSection.insertAdjacentHTML("afterbegin", recipeCardDOM);
 
     const recipeDetails = document.querySelector(".recipe_ingredients");
-    // recipeModel = recipeFactory(data);
     const ingredientDOM = recipeModel.getIngredientsList();
     recipeDetails.appendChild(ingredientDOM);
   });
@@ -22,3 +23,14 @@ async function init() {
 }
 
 init();
+
+//------------------------------------------------------------------------------------------
+//Event Listener
+const searchBar = document.querySelector("#search_recipe");
+searchBar.addEventListener("keyup", (e) => {
+  const value = e.target.value;
+  const regex = /[A-Za-z0-9]{3,}/;
+  if (regex.test(value)) {
+    searchRecipe(value);
+  }
+});
