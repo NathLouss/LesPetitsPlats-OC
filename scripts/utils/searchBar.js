dd187262ff81b5e7f356a5213a04cb58a4b231bd
nathalie loussouarn <natel@hotmail.com>
2022-12-08 15:47:17 +0100

    sort test on filteredDatas

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -120,3 +120,35 @@ export function filterDatas(value, datas) {
 //   console.log(filteredDatas.indexOf(recipe.id));
 //   filteredDatas.push(recipe);
 // }
+
+function dynamicSort(property) {
+  var sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a, b) {
+    /* next line works with strings and numbers,
+     * and you may want to customize it to your needs
+     */
+    var result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result * sortOrder;
+  };
+}
+
+// function dynamicSort(property) {
+//   var sortOrder = 1;
+//   if (property[0] === "-") {
+//     sortOrder = -1;
+//     property = property.substr(1);
+//   }
+//   return function (a, b) {
+//     /* next line works with strings and numbers,
+//      * and you may want to customize it to your needs
+//      */
+//     var result =
+//       a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+//     return result * sortOrder;
+//   };
+// }
