efbc16a07969fa9e085b35660afc181a79c0b3c6
nathalie loussouarn <natel@hotmail.com>
2022-12-08 19:02:07 +0100

    correct format datas in filter function and sort function

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -8,17 +8,51 @@ export function filterDatas(value, datas) {
     .replace(/[\u0300-\u036f]/g, "");
   filteredDatas = datas.filter(
     (data) =>
-      data.name.toLowerCase().includes(inputFormated) ||
-      data.appliance.toLowerCase().includes(inputFormated) ||
-      data.description.toLowerCase().includes(inputFormated) ||
+      data.name
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .includes(inputFormated) ||
+      data.appliance
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .includes(inputFormated) ||
+      data.description
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .includes(inputFormated) ||
       data.ustensils.forEach((ustensil) =>
-        ustensil.toLowerCase().includes(inputFormated)
+        ustensil
+          .toLowerCase()
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .includes(inputFormated)
       ) ||
       data.ingredients.forEach((ingredient) =>
-        ingredient.ingredient.toLowerCase().includes(inputFormated)
+        ingredient.ingredient
+          .toLowerCase()
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .includes(inputFormated)
       )
   );
+  sortDatas(filteredDatas);
   return filteredDatas;
 }
 
-function sortFilterDatas(filteredDatas) {}
+function sortDatas(filteredDatas) {
+  filteredDatas.sort(function (a, b) {
+    let x = a.name.toLowerCase();
+    let y = b.name.toLowerCase();
+    if (x > y) {
+      return 1;
+    }
+    if (x < y) {
+      return -1;
+    }
+    return 0;
+  });
+  return filteredDatas;
+}
