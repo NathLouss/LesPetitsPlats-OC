e7bc8f90b27bca0f32a9fb81b4bc93106c82bc9f
nathalie loussouarn <natel@hotmail.com>
2022-12-08 12:01:03 +0100

    fixed an issue with the condition to only push the recipe not present in filteredDatas

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,90 +1,98 @@
 let filteredDatas = [];
 
 export function filterDatas(value, datas) {
-  console.log(datas);
+  filteredDatas = [];
   const inputFormated = value
     .toLowerCase()
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
-  filteredDatas = [];
-
-  // debugger;
-  // const keys = Object.keys(datas[i]);
-  // console.log(keys);
-  // for (const key in keys) {
-  // }
 
   // pour chaque recette, on va comparer le nom à l'input
-  // for (let i = 0; i < datas.length; i++) {
-  //   const nameFormated = datas[i].name
-  //     .toLowerCase()
-  //     .normalize("NFD")
-  //     .replace(/[\u0300-\u036f]/g, "");
-  //   // si l'input match avec un mot du nom de la recette
-  //   if (nameFormated.includes(inputFormated)) {
-  //     const matchingRecipe = datas[i];
-  //     // si le tableau filteredDatas est vide
-  //     if (filteredDatas.length === 0) {
-  //       // la matching recette est envoyée dans le tableau filteredDatas
-  //       filteredDatas.push(matchingRecipe);
-  //       // sinon si la matching recette est différente de celle présente dans filteredDatas
-  //     } else if (!(filteredDatas.indexOf(matchingRecipe.name) !== -1)) {
-  //       // la matching recette est envoyée dans le tableau filteredDatas
-  //       filteredDatas.push(matchingRecipe);
-  //     }
-  //   }
-  // }
+  for (let i = 0; i < datas.length; i++) {
+    const recipe = datas[i];
+    const nameFormated = datas[i].name
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    // si l'input match avec un mot du nom de la recette
+    if (nameFormated.includes(inputFormated)) {
+      // on définit une variable pour vérifier si une autre recette
+      // est déjà présente dans filteredDatas avec le même Id
+      const checkId = filteredDatas.some(
+        (filteredData) => filteredData.id === recipe.id
+      );
+      // si le tableau filteredDatas est vide
+      if (filteredDatas.length === 0) {
+        // la recette est envoyée dans le tableau filteredDatas
+        filteredDatas.push(recipe);
+        // sinon, si la recette n'est pas présente dans filteredDatas
+      } else if (!checkId) {
+        // la recette est envoyée dans le tableau filteredDatas
+        filteredDatas.push(recipe);
+      }
+    }
+  }
   // pour chaque recette, on va comparer la description à l'input
-  // for (let i = 0; i < datas.length; i++) {
-  //   const descriptionFormated = datas[i].description
-  //     .toLowerCase()
-  //     .normalize("NFD")
-  //     .replace(/[\u0300-\u036f]/g, "");
-  //   if (descriptionFormated.includes(inputFormated)) {
-  //     const matchingRecipe = datas[i];
-  //     if (filteredDatas.length === 0) {
-  //       filteredDatas.push(matchingRecipe);
-  //     } else if (!(filteredDatas.indexOf(matchingRecipe.name) !== -1)) {
-  //       filteredDatas.push(matchingRecipe);
-  //     }
-  //   }
-  // }
+  for (let i = 0; i < datas.length; i++) {
+    const recipe = datas[i];
+    const descriptionFormated = datas[i].description
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    if (descriptionFormated.includes(inputFormated)) {
+      const checkId = filteredDatas.some(
+        (filteredData) => filteredData.id === recipe.id
+      );
+      if (filteredDatas.length === 0) {
+        filteredDatas.push(recipe);
+      } else if (!checkId) {
+        filteredDatas.push(recipe);
+      }
+    }
+  }
   // pour chaque recette, on va comparer l'appareil à l'input
-  // for (let i = 0; i < datas.length; i++) {
-  //   const applianceFormated = datas[i].appliance
-  //     .toLowerCase()
-  //     .normalize("NFD")
-  //     .replace(/[\u0300-\u036f]/g, "");
-  //   if (applianceFormated.includes(inputFormated)) {
-  //     const matchingRecipe = datas[i];
-  //     if (filteredDatas.length === 0) {
-  //       filteredDatas.push(matchingRecipe);
-  //     } else if (!(filteredDatas.indexOf(matchingRecipe.name) !== -1)) {
-  //       filteredDatas.push(matchingRecipe);
-  //     }
-  //   }
-  // }
+  for (let i = 0; i < datas.length; i++) {
+    const recipe = datas[i];
+    const applianceFormated = datas[i].appliance
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    if (applianceFormated.includes(inputFormated)) {
+      const checkId = filteredDatas.some(
+        (filteredData) => filteredData.id === recipe.id
+      );
+      if (filteredDatas.length === 0) {
+        filteredDatas.push(recipe);
+      } else if (!checkId) {
+        filteredDatas.push(recipe);
+      }
+    }
+  }
   // pour chaque recette, on va récupérer les ingrédients
-  // for (let i = 0; i < datas.length; i++) {
-  //   const ingredientsArr = datas[i].ingredients;
-  //   // pour chaque recette, on va comparer les ingrédients à l'input
-  //   for (let i = 0; i < ingredientsArr.length; i++) {
-  //     const ingredientFormated = ingredientsArr[i].ingredient
-  //       .toLowerCase()
-  //       .normalize("NFD")
-  //       .replace(/[\u0300-\u036f]/g, "");
-  //     if (ingredientFormated.includes(inputFormated)) {
-  //       const matchingRecipe = datas[i];
-  //       if (filteredDatas.length === 0) {
-  //         filteredDatas.push(matchingRecipe);
-  //       } else if (!(filteredDatas.indexOf(matchingRecipe.name) !== -1)) {
-  //         filteredDatas.push(matchingRecipe);
-  //       }
-  //     }
-  //   }
-  // }
+  for (let i = 0; i < datas.length; i++) {
+    const recipe = datas[i];
+    const ingredientsArr = datas[i].ingredients;
+    // pour chaque recette, on va comparer les ingrédients à l'input
+    for (let i = 0; i < ingredientsArr.length; i++) {
+      const ingredientFormated = ingredientsArr[i].ingredient
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+      if (ingredientFormated.includes(inputFormated)) {
+        const checkId = filteredDatas.some(
+          (filteredData) => filteredData.id === recipe.id
+        );
+        if (filteredDatas.length === 0) {
+          filteredDatas.push(recipe);
+        } else if (!checkId) {
+          filteredDatas.push(recipe);
+        }
+      }
+    }
+  }
   // pour chaque recette, on va récupérer les ustensiles
   for (let i = 0; i < datas.length; i++) {
+    const recipe = datas[i];
     const ustensilsArr = datas[i].ustensils;
     // pour chaque recette, on va comparer les ingrédients à l'input
     for (let i = 0; i < ustensilsArr.length; i++) {
@@ -93,14 +101,21 @@ export function filterDatas(value, datas) {
         .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "");
       if (ustensilsFormated.includes(inputFormated)) {
-        const matchingRecipe = datas[i];
+        const checkId = filteredDatas.some(
+          (filteredData) => filteredData.id === recipe.id
+        );
         if (filteredDatas.length === 0) {
-          filteredDatas.push(matchingRecipe);
-        } else if (!(filteredDatas.indexOf(matchingRecipe.name) !== -1)) {
-          filteredDatas.push(matchingRecipe);
+          filteredDatas.push(recipe);
+        } else if (!checkId) {
+          filteredDatas.push(recipe);
         }
       }
     }
   }
   console.log(filteredDatas);
 }
+
+// } else if (!(filteredDatas.indexOf(recipe.id) !== -1)) {
+//   console.log(filteredDatas.indexOf(recipe.id));
+//   filteredDatas.push(recipe);
+// }
