7845b469d92405a5f6e3e2e879306977a6200768
nathalie loussouarn <natel@hotmail.com>
2022-12-08 18:40:05 +0100

    functional filtering algorithm

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,3 +1,24 @@
-export function searchRecipe(value) {
-  console.log("ok", value);
+let filteredDatas = [];
+
+export function filterDatas(value, datas) {
+  filteredDatas.innerHTML = "";
+  const inputFormated = value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  filteredDatas = datas.filter(
+    (data) =>
+      data.name.toLowerCase().includes(inputFormated) ||
+      data.appliance.toLowerCase().includes(inputFormated) ||
+      data.description.toLowerCase().includes(inputFormated) ||
+      data.ustensils.forEach((ustensil) =>
+        ustensil.toLowerCase().includes(inputFormated)
+      ) ||
+      data.ingredients.forEach((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(inputFormated)
+      )
+  );
+  return filteredDatas;
 }
+
+function sortFilterDatas(filteredDatas) {}
