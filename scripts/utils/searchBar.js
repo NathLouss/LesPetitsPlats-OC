f0fec99efd1b13b923fd1876ea2a677bd747e58e
nathalie loussouarn <natel@hotmail.com>
2022-12-09 11:33:55 +0100

    reflection on native sorting

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -2,10 +2,40 @@ let filteredDatas = [];
 
 export function filterDatas(value, datas) {
   filteredDatas = [];
+  // const { elements } = [name, ingredients, description, appliance, ustensils];
   const inputFormated = value
     .toLowerCase()
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
+
+  // pour chaque recette, on va comparer le nom à l'input
+  // for (let i = 0; i < datas.length; i++) {
+  //   const recipe = datas[i];
+  //   const elements = Object.keys(recipe);
+  //   console.log(elements);
+  //   for (let i = 0; i < elements.length; i++) {
+  //     const key = elements[i];
+  //     datas[i][key] = elements[i];
+  //     // const stringToKey = {
+  //     //   [keyName]: elements[i],
+  //     // };
+  //     console.log(datas[i].keyName);
+  //     const elementFormated = datas[i].elements[i]
+  //       .toLowerCase()
+  //       .normalize("NFD")
+  //       .replace(/[\u0300-\u036f]/g, "");
+  //     if (elementFormated.includes(inputFormated)) {
+  //       const checkId = filteredDatas.some(
+  //         (filteredData) => filteredData.id === recipe.id
+  //       );
+  //       if (filteredDatas.length === 0) {
+  //         filteredDatas.push(recipe);
+  //       } else if (!checkId) {
+  //         filteredDatas.push(recipe);
+  //       }
+  //     }
+  //   }
+  // }
 
   // pour chaque recette, on va comparer le nom à l'input
   for (let i = 0; i < datas.length; i++) {
@@ -112,30 +142,22 @@ export function filterDatas(value, datas) {
       }
     }
   }
-  // console.log(filteredDatas);
+  // sortDatas(datas, `name`);
   return filteredDatas;
 }
-
-// } else if (!(filteredDatas.indexOf(recipe.id) !== -1)) {
-//   console.log(filteredDatas.indexOf(recipe.id));
-//   filteredDatas.push(recipe);
+// function sortDatas(array, key) {
+//   console.log(key);
+//   for (let i = 0; i < array.length; i++) {
+//     const currVal = array[i][key];
+//     const currElem = array[i];
+//     const j = i - 1;
+//     while (j >= 0 && array[j][key] > currVal) {
+//       array[j + 1] = array[j];
+//       // j–;
+//     }
+//     array[j + 1] = currElem;
+//   }
 // }
-
-function dynamicSort(property) {
-  var sortOrder = 1;
-  if (property[0] === "-") {
-    sortOrder = -1;
-    property = property.substr(1);
-  }
-  return function (a, b) {
-    /* next line works with strings and numbers,
-     * and you may want to customize it to your needs
-     */
-    var result =
-      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
-    return result * sortOrder;
-  };
-}
 
 // function dynamicSort(property) {
 //   var sortOrder = 1;
@@ -152,3 +174,46 @@ function dynamicSort(property) {
 //     return result * sortOrder;
 //   };
 // }
+
+// var items = [5,3,7,6,2,9];
+// function swap(items, leftIndex, rightIndex){
+//     var temp = items[leftIndex];
+//     items[leftIndex] = items[rightIndex];
+//     items[rightIndex] = temp;
+// }
+// function partition(items, left, right) {
+//     var pivot   = items[Math.floor((right + left) / 2)], //middle element
+//         i       = left, //left pointer
+//         j       = right; //right pointer
+//     while (i <= j) {
+//         while (items[i] < pivot) {
+//             i++;
+//         }
+//         while (items[j] > pivot) {
+//             j--;
+//         }
+//         if (i <= j) {
+//             swap(items, i, j); //sawpping two elements
+//             i++;
+//             j--;
+//         }
+//     }
+//     return i;
+// }
+
+// function quickSort(items, left, right) {
+//     var index;
+//     if (items.length > 1) {
+//         index = partition(items, left, right); //index returned from partition
+//         if (left < index - 1) { //more elements on the left side of the pivot
+//             quickSort(items, left, index - 1);
+//         }
+//         if (index < right) { //more elements on the right side of the pivot
+//             quickSort(items, index, right);
+//         }
+//     }
+//     return items;
+// }
+// // first call to quick sort
+// var sortedArray = quickSort(items, 0, items.length - 1);
+// console.log(sortedArray); //prints [2,3,5,6,7,9]
