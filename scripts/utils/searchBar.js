2e813f7edb29edafb6a084bbe9af7c6b78f4d0c5
nathalie loussouarn <natel@hotmail.com>
2022-12-09 15:41:32 +0100

    native loop sort function

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -7,7 +7,6 @@ export function filterDatas(value, datas) {
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
 
-  // pour chaque recette, on va comparer le nom à l'input
   for (let i = 0; i < datas.length; i++) {
     const recipe = datas[i];
     const nameFormated = recipe.name
@@ -22,18 +21,15 @@ export function filterDatas(value, datas) {
       .toLowerCase()
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "");
-    // si l'input match avec un mot du nom de la recette
+    // pour chaque recette, on va comparer le nom à l'input
     if (nameFormated.includes(keyword)) {
-      // on définit une variable pour vérifier si une autre recette
-      // est déjà présente dans filteredDatas avec le même Id
       const checkId = filteredDatas.some(
         (filteredData) => filteredData.id === recipe.id
       );
-      // si la recette n'est pas présente dans filteredDatas
       if (!checkId) {
-        // la recette est envoyée dans le tableau filteredDatas
         filteredDatas.push(recipe);
       }
+      // pour chaque recette, on va comparer la description à l'input
     } else if (descriptionFormated.includes(keyword)) {
       const checkId = filteredDatas.some(
         (filteredData) => filteredData.id === recipe.id
@@ -41,6 +37,7 @@ export function filterDatas(value, datas) {
       if (!checkId) {
         filteredDatas.push(recipe);
       }
+      // pour chaque recette, on va comparer l'appareil à l'input
     } else if (applianceFormated.includes(keyword)) {
       const checkId = filteredDatas.some(
         (filteredData) => filteredData.id === recipe.id
@@ -49,8 +46,8 @@ export function filterDatas(value, datas) {
         filteredDatas.push(recipe);
       }
     } else {
-      const ingredientsArr = recipe.ingredients;
       // pour chaque recette, on va comparer les ingrédients à l'input
+      const ingredientsArr = recipe.ingredients;
       for (let j = 0; j < ingredientsArr.length; j++) {
         const ingredientFormated = ingredientsArr[j].ingredient
           .toLowerCase()
@@ -65,8 +62,8 @@ export function filterDatas(value, datas) {
           }
         }
       }
-      const ustensilsArr = recipe.ustensils;
       // pour chaque recette, on va comparer les ingrédients à l'input
+      const ustensilsArr = recipe.ustensils;
       for (let i = 0; i < ustensilsArr.length; i++) {
         const ustensilsFormated = ustensilsArr[i]
           .toLowerCase()
@@ -83,80 +80,19 @@ export function filterDatas(value, datas) {
       }
     }
   }
-
-  // sortDatas(datas, `name`);
+  sortDatas(datas, `name`);
   return filteredDatas;
 }
 
-// function sortDatas(array, key) {
-//   console.log(key);
-//   for (let i = 0; i < array.length; i++) {
-//     const currVal = array[i][key];
-//     const currElem = array[i];
-//     const j = i - 1;
-//     while (j >= 0 && array[j][key] > currVal) {
-//       array[j + 1] = array[j];
-//       // j–;
-//     }
-//     array[j + 1] = currElem;
-//   }
-// }
-
-// function dynamicSort(property) {
-//   var sortOrder = 1;
-//   if (property[0] === "-") {
-//     sortOrder = -1;
-//     property = property.substr(1);
-//   }
-//   return function (a, b) {
-//     /* next line works with strings and numbers,
-//      * and you may want to customize it to your needs
-//      */
-//     var result =
-//       a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
-//     return result * sortOrder;
-//   };
-// }
-
-// var items = [5,3,7,6,2,9];
-// function swap(items, leftIndex, rightIndex){
-//     var temp = items[leftIndex];
-//     items[leftIndex] = items[rightIndex];
-//     items[rightIndex] = temp;
-// }
-// function partition(items, left, right) {
-//     var pivot   = items[Math.floor((right + left) / 2)], //middle element
-//         i       = left, //left pointer
-//         j       = right; //right pointer
-//     while (i <= j) {
-//         while (items[i] < pivot) {
-//             i++;
-//         }
-//         while (items[j] > pivot) {
-//             j--;
-//         }
-//         if (i <= j) {
-//             swap(items, i, j); //sawpping two elements
-//             i++;
-//             j--;
-//         }
-//     }
-//     return i;
-// }
-
-// function quickSort(items, left, right) {
-//     var index;
-//     if (items.length > 1) {
-//         index = partition(items, left, right); //index returned from partition
-//         if (left < index - 1) { //more elements on the left side of the pivot
-//             quickSort(items, left, index - 1);
-//         }
-//         if (index < right) { //more elements on the right side of the pivot
-//             quickSort(items, index, right);
-//         }
-//     }
-//     return items;
-// }
-// // first call to quick sort
-// var sortedArray = quickSort(items, 0, items.length - 1);
-// console.log(sortedArray); //prints [2,3,5,6,7,9]
+function sortDatas(array, key) {
+  for (let i = 0; i < array.length; i++) {
+    const currVal = array[i][key];
+    const currElem = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j][key] > currVal) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currElem;
+  }
+}
