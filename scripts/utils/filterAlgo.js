501900213ff19ec342dda81d41abd5e3e0b7f18d
nathalie loussouarn <natel@hotmail.com>
2022-12-15 15:17:56 +0100

    prettier behavior correction

--- scripts/utils/filterAlgo.js
+++ scripts/utils/filterAlgo.js
@@ -1,57 +1,57 @@
-let filteredDatas = [];
+let filteredDatas = []
 
 export function filterDatas(value, datas) {
-  filteredDatas.innerHTML = "";
-  const inputFormated = value
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
-  filteredDatas = datas.filter(
-    (data) =>
-      data.name
-        .toLowerCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .includes(inputFormated) ||
-      data.ingredients
-        .map((ingredients) => ingredients.ingredient)
-        .toString()
-        .toLowerCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .includes(inputFormated) ||
-      data.description
-        .toLowerCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .includes(inputFormated) ||
-      data.appliance
-        .toLowerCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .includes(inputFormated) ||
-      data.ustensils
-        .toString()
-        .toLowerCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .includes(inputFormated)
-  );
-  sortDatas(filteredDatas);
-  return filteredDatas;
+	filteredDatas.innerHTML = ''
+	const inputFormated = value
+		.toLowerCase()
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
+	filteredDatas = datas.filter(
+		(data) =>
+			data.name
+				.toLowerCase()
+				.normalize('NFD')
+				.replace(/[\u0300-\u036f]/g, '')
+				.includes(inputFormated) ||
+			data.ingredients
+				.map((ingredients) => ingredients.ingredient)
+				.toString()
+				.toLowerCase()
+				.normalize('NFD')
+				.replace(/[\u0300-\u036f]/g, '')
+				.includes(inputFormated) ||
+			data.description
+				.toLowerCase()
+				.normalize('NFD')
+				.replace(/[\u0300-\u036f]/g, '')
+				.includes(inputFormated) ||
+			data.appliance
+				.toLowerCase()
+				.normalize('NFD')
+				.replace(/[\u0300-\u036f]/g, '')
+				.includes(inputFormated) ||
+			data.ustensils
+				.toString()
+				.toLowerCase()
+				.normalize('NFD')
+				.replace(/[\u0300-\u036f]/g, '')
+				.includes(inputFormated)
+	)
+	sortDatas(filteredDatas)
+	return filteredDatas
 }
 
 export function sortDatas(filteredDatas) {
-  filteredDatas.sort(function (a, b) {
-    let x = a.name.toLowerCase();
-    let y = b.name.toLowerCase();
-    if (x > y) {
-      return 1;
-    }
-    if (x < y) {
-      return -1;
-    }
-    return 0;
-  });
-  return filteredDatas;
+	filteredDatas.sort(function (a, b) {
+		let x = a.name.toLowerCase()
+		let y = b.name.toLowerCase()
+		if (x > y) {
+			return 1
+		}
+		if (x < y) {
+			return -1
+		}
+		return 0
+	})
+	return filteredDatas
 }
