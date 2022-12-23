0465abe717640a77c5cb3aed0ea7b697e7c8e784
nathalie loussouarn <natel@hotmail.com>
2022-12-23 15:26:35 +0100

    toggle dropdown function

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -1,23 +1,28 @@
 export function toggleDropDown(e) {
-	const allList = document.querySelectorAll('.filter_list')
-	debugger
-	allList.forEach((list) => {
-		list.style.display = 'none'
-	})
 	const allBtn = document.querySelectorAll('.filter_btn')
 	allBtn.forEach((btn) => {
 		btn.style.display = 'block'
+	})
+	const allForm = document.querySelectorAll('.filter_form')
+	allForm.forEach((form) => {
+		form.style.display = 'none'
 	})
 	const allInput = document.querySelectorAll('.filter_input')
 	allInput.forEach((input) => {
 		input.classList.remove('filter_input_active')
 	})
-	if (e.currentTarget.className != 'filter_close') {
-		const btnClicked = document.getElementById(`filter_btn_${selectedFilter}`)
+	const allList = document.querySelectorAll('.filter_list')
+	allList.forEach((list) => {
+		list.style.display = 'none'
+	})
+	if (!e.currentTarget.className.includes('filter_close')) {
+		const btnClicked = e.currentTarget
 		btnClicked.style.display = 'none'
-		const filterSelected = document.getElementById(`filter_by_${selectedFilter}`)
-		filterSelected.style.display = 'block'
-		const inputSelected = document.getElementById(`input_${selectedFilter}`)
+		const formSelected = e.currentTarget.nextElementSibling
+		formSelected.style.display = 'block'
+		const inputSelected = e.currentTarget.nextElementSibling[0]
 		inputSelected.classList.add('filter_input_active')
+		const listSelected = e.currentTarget.nextElementSibling.nextElementSibling
+		listSelected.style.display = 'block'
 	}
 }
