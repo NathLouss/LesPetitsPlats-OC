b39caf96e89d980b5b9ba1c6c1a8b3b6fe7d614d
nathalie loussouarn <natel@hotmail.com>
2023-01-11 18:42:37 +0100

    functional tag filter

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ import { recipeFactory } from './factory/recipeFactory.js'
 import { filterFactory } from './factory/filterFactory.js'
 import { filterRecipes, sortRecipes } from './utils/filterAlgo.js'
 import { toggleDropDown } from './utils/dropdown.js'
-import { filterByTag } from './utils/tag.js'
+// import { filterByTag } from './utils/tag.js'
 
 // déclaration variables
 let recipes = []
@@ -12,7 +12,7 @@ let appliancesList = []
 let ustensilsList = []
 let lists = []
 let filteredRecipes = []
-export let searchBarKeyword = []
+let searchBarKeyword = []
 
 // création et affichage des cards recette via la recipeFactory
 function displayRecipes(recipes) {
@@ -76,7 +76,7 @@ function groupLists() {
 	}
 }
 
-export function listInit(recipes) {
+function listInit(recipes) {
 	createListIngredients(recipes)
 	createListAppliances(recipes)
 	createListUstensils(recipes)
@@ -98,7 +98,7 @@ function displayFilterList(lists, keyword = null) {
 			// on postionne un eventlistener sur chaque option de filtre li
 			Object.values(filterListCardDOM).forEach((li) => {
 				li.addEventListener('click', (e) => {
-					filterByTag(e, filterName, recipes)
+					createTag(e, filterName, recipes)
 				})
 				ulSection.appendChild(li)
 			})
@@ -122,7 +122,7 @@ function displayFilterList(lists, keyword = null) {
 			// on postionne un eventlistener sur chaque option de filtre li
 			Object.values(filterListCardDOM).forEach((li) => {
 				li.addEventListener('click', (e) => {
-					filterByTag(e, filterName, recipes)
+					createTag(e, filterName, recipes)
 				})
 				ulSection.appendChild(li)
 			})
@@ -150,10 +150,10 @@ const searchBar = document.querySelector('#search_recipe')
 const recipesSection = document.getElementById('recipes')
 const cross = document.querySelector('.search_btn_cross')
 const searchBarForm = document.querySelector('form')
+const recipesLength = document.querySelector('.search_recipe_number')
 
 searchBar.addEventListener('input', (e) => {
 	const value = e.target.value
-	const recipesLength = document.querySelector('.search_recipe_number')
 
 	if (value.length >= 3) {
 		initResetSearchbar()
@@ -240,7 +240,7 @@ inputsFilter.forEach((input) =>
 				liFilter.textContent = eltFormated
 				// on postionne un eventlistener sur chaque option de filtre li
 				liFilter.addEventListener('click', (e) => {
-					filterByTag(e, filterProperty, recipes)
+					createTag(e, filterProperty, recipes)
 				})
 				ulSection.appendChild(liFilter)
 			})
@@ -278,4 +278,88 @@ function initResetInput(e) {
 			init()
 		}
 	})
+}
+
+//------------------------------------------------------------------------------------------
+// Gestion des tags
+let tagList = []
+let globalKeyword = []
+let filteredRecipesByTag = []
+
+function createTag(e, selectedFilter) {
+	const filterTag = document.getElementById('filters_tags')
+	filterTag.classList.add('filters_tags_active')
+
+	const tagDiv = document.createElement('div')
+	tagDiv.classList.add('filter_tag_div')
+	tagDiv.classList.add(`color_tag_${selectedFilter}`)
+	filterTag.appendChild(tagDiv)
+
+	const tag = document.createElement('p')
+	tag.classList.add('filter_tag_p')
+	tag.textContent = e.target.innerText
+	tagDiv.appendChild(tag)
+
+	const tagIcon = document.createElement('i')
+	tagIcon.classList.add('far', 'fa-times-circle')
+	tagIcon.addEventListener('click', (e) => deleteTag(e))
+	tagDiv.appendChild(tagIcon)
+
+	const filterInput = document.getElementById(`input_${selectedFilter}`)
+	filterInput.value = ''
+	const filterListInput = document.getElementById(`filter_by_${selectedFilter}`)
+	filterListInput.style.display = 'none'
+	const filterList = document.getElementById(`filter_list_${selectedFilter}`)
+	filterList.style.display = 'none'
+	const filterButton = document.getElementById(`filter_btn_${selectedFilter}`)
+	filterButton.style.display = 'block'
+
+	// ajoute le tag dans tagList
+	tagList.push(e.target.innerText)
+	// concatene les 2 tableaux de keyword
+	globalKeyword = searchBarKeyword.concat(tagList)
+	filterByKeyword(recipes, globalKeyword)
+}
+
+function deleteTag(e) {
+	e.target.parentElement.remove()
+	const tagDiv = document.getElementById('filters_tags')
+	if (tagDiv.innerHTML === '') {
+		tagDiv.classList.remove('filters_tags_active')
+	}
+
+	// supprime le tag de tagList
+	const index = tagList.indexOf(e.target.previousSibling.innerHTML)
+	if (index > -1) {
+		tagList.splice(index, 1)
+	}
+
+	// supprime le tag de globalKeyword
+	const index2 = globalKeyword.indexOf(e.target.previousSibling.innerHTML)
+	if (index2 > -1) {
+		globalKeyword.splice(index2, 1)
+	}
+
+	filterByKeyword(recipes, globalKeyword)
+}
+
+function filterByKeyword(recipes, globalKeyword) {
+	if (globalKeyword.length != 0) {
+		filteredRecipesByTag = recipes
+		// filtre les recettes avec chaque keyword
+		globalKeyword.forEach((keyword) => {
+			filteredRecipesByTag = filterRecipes(keyword, filteredRecipesByTag)
+			// affiche le nombre de recettes filtrées
+			recipesLength.textContent = filteredRecipesByTag.length
+			// affiche les recettes filtrées
+			displayRecipes(filteredRecipesByTag)
+			// réinitialise les listes ingr, app, ustens avec les recettes filtrées
+			listInit(filteredRecipesByTag)
+			// affiche les listes des recettes filtrées
+			displayFilterList(lists)
+		})
+	} else {
+		init()
+		document.querySelector('.search_recipe_number').textContent = '50'
+	}
 }
